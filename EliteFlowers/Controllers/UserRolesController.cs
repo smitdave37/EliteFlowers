cfc857e9e58933b8fff8e6f217dd4827b1dbb793
Smit Jayeshkumar Dave <smitdave37@gmail.com>
2024-03-15 21:08:40 -0400

    Previous change

--- EliteFlowers/Controllers/UserRolesController.cs
+++ EliteFlowers/Controllers/UserRolesController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EliteFlowers.Controllers
 {
-    [Authorize(Roles = "Admin")]
+   // [Authorize(Roles = "Admin")]
     public class UserRolesController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleManager;
